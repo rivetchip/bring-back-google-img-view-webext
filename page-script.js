7eb158ed6f0850ab222f34894610248c4520bb2d
rivetchip <me@spidery.fr>
2018-02-17 14:42:03 +0100

    mega fix for not loaded immersive

--- page-script.js
+++ page-script.js
@@ -4,11 +4,30 @@
 var buttonId = 'button-google-view-image-'+(Math.random().toString(36).substr(2, 10));
 var isClickEventOnParent = false; // once the popup is loaded
 
-// all pictures block container
-var imagesContainer = document.querySelector('#rg_s');
+var doc = {
+    imagesContainer: '#rg_s', // all pictures block container
+    imagesContainerDelegate: 'a.rg_l, img.rg_ic',
+
+    immersiveContainerParent: '#irc_bg', // black popup container of immersives
+    immersiveContainerParentDelegate: 'a.rg_l, img.target_image, img.irc_rii',
+
+    immersiveContainers: '#irc_bg .immersive-container', // over top panels
+    imagePanel: '.irc_mi', // preview image
+    descPanel: '.irc_mmc table.irc_but_r tr td' // website desc & url
+};
+
+
+var imagesContainer = document.querySelector(doc.imagesContainer);
 
 // main blocks ; click event
-delegate(imagesContainer, 'a.rg_l, img.rg_ic', 'click', onImmersiveContainerAction);
+delegate(imagesContainer, doc.imagesContainerDelegate, 'click', onImmersiveContainerAction);
+
+// black popup ; first time : not loaded !!
+var immersiveContainerParent = document.querySelector(doc.immersiveContainerParent);
+if( immersiveContainerParent ) {
+    // similares pictures ; click event
+    delegate(immersiveContainerParent, doc.immersiveContainerParentDelegate, 'click', onImmersiveContainerAction);
+}
 
 // from the /imgres direct page
 onImmersiveContainerAction();
@@ -16,18 +35,17 @@ onImmersiveContainerAction();
 
 
 function onImmersiveContainerAction( event ) {
-    // var target = event ? event.target : null;
+    var target = event ? event.target : null;
     var immersiveContainers;
 
     setTimeout(function(){
 
-        // over top panels (3)
-        immersiveContainers = document.querySelectorAll('#irc_bg .immersive-container');
+        immersiveContainers = document.querySelectorAll(doc.immersiveContainers);
 
         for( var immersiveContainer of immersiveContainers ) {
 
-            var imagePanel = immersiveContainer.querySelector('.irc_mi'); // preview image
-            var descPanel = immersiveContainer.querySelector('.irc_mmc table.irc_but_r tr td'); // website desc & url
+            var imagePanel = immersiveContainer.querySelector(doc.imagePanel);
+            var descPanel = immersiveContainer.querySelector(doc.descPanel);
 
             if( imagePanel && descPanel ) {
                 var button = createButtonFrom(immersiveContainer, imagePanel);
@@ -38,14 +56,14 @@ function onImmersiveContainerAction( event ) {
 
         // add click event on parent ; aka for similar pictures :
 
-        if( !isClickEventOnParent ) {
+        if( target && !isClickEventOnParent ) {
 
-            // black popup container of immersives
-            var immersiveContainerParent = document.querySelector('#irc_bg');
-
-            // similares pictures ; click event
-            delegate(immersiveContainerParent, 'a.rg_l, img.target_image, img.irc_rii', 'click', onImmersiveContainerAction);
-
+            // black popup container of immersives ; first time : not loaded !!
+            immersiveContainerParent = document.querySelector(doc.immersiveContainerParent);
+            if( immersiveContainerParent ) {
+                // similares pictures ; click event
+                delegate(immersiveContainerParent, doc.immersiveContainerParentDelegate, 'click', onImmersiveContainerAction);
+            }
 
             isClickEventOnParent = true;
         }
